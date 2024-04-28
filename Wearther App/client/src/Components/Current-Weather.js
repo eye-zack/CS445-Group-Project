a2cd183449f87881ca6d511fa0be258388872ba2
eye-zack <isaacray649@gmail.com>
2024-04-28 09:32:56 -0500

    Weather container template created and formatted
    The weather api has not been implemented yet, but the template for weather is put in place. Added the icon that Justin created to the index.html file.

--- Wearther App/client/src/Components/Current-Weather.js
+++ Wearther App/client/src/Components/Current-Weather.js
@@ -13,6 +13,30 @@ const CurrentWeather = () => {
           src="WeatherIcons/clear-day.svg"
         ></img>
       </div>
+      <div className="bottom">
+        <p className="temperature">75°F</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value">78°F</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">15 MPH</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">71%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">30.00 inHg</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
